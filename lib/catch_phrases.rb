bd619604d02bcfef135c4b3c1d8c8bdf60821d38
David Lee <david.lee.sl2634@yale.edu>
2020-05-09 01:52:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,24 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+
+def all_phrases
+  mario
+  toadstool
+  link
 end
 
-def toadstool
-  puts status
+def mario
+  phrase = "It's-a me, Mario!"
+  puts phrase
 end
+phrase
+
+def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts status
+end 
+status
+
+def link
+  statement = "It's Dangerous To Go ALone! Take This."
+  puts statement
+end
+statement
